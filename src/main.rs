2bf6506b48c0e37f7a9df6cee555d8d62ef50917
rot1024 <aayhrot@gmail.com>
2020-06-18 02:36:36 +0900

    load config

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
 use actix_web::{get, web, App, HttpServer, Responder};
+use serde::Deserialize;
+
+fn default_port() -> u16 {
+    3000
+}
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    #[serde(default = "default_port")]
+    port: u16,
+}
 
 #[get("/{id}/{name}")]
 async fn index(info: web::Path<(String, String)>) -> impl Responder {
@@ -7,8 +18,10 @@ async fn index(info: web::Path<(String, String)>) -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    let config = envy::from_env::<Config>().expect("Failed to load config.");
+
     HttpServer::new(|| App::new().service(index))
-        .bind("127.0.0.1:8080")?
+        .bind(("127.0.0.1", config.port))?
         .run()
         .await
 }
